b9968ff162d469a5720403f83b27b991caedc7e0
Alkerg <albert.arg500@gmail.com>
2023-10-08 14:58:30 -0500

    Changes in Player.cs to move the camera
    Raycast 2D implemented

--- Proyecto-POO/Assets/Scripts/Player.cs
+++ Proyecto-POO/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -10,8 +11,11 @@ public class Player : MonoBehaviour
     private Transform firePivot;
     [SerializeField]
     private GameObject bulletPrefab;
-    
-    
+    public AnimationCurve curve;
+    public Camera gameCamera;
+    public float lerpDuration;
+    private Vector3 gameCameraVector;
+
     #endregion
     #region Movement and Physics
     [Header("Movement and Physics")]
@@ -32,12 +36,19 @@ public class Player : MonoBehaviour
     #region Events
     void Start()
     {
+        gameCamera = Camera.main;
         rb = GetComponent<Rigidbody2D>();
+
+        float gameCameraHeight = 2 * gameCamera.orthographicSize;
+        float gameCameraWidth = gameCameraHeight * gameCamera.aspect;
+        gameCameraVector = new Vector3(gameCameraWidth, gameCameraHeight, gameCamera.transform.position.z);
+        lerpDuration = 0.5f;
     }
     private void Update()
     {
         GetInput();
         Shoot();
+        DetectDoor();
     }
     void FixedUpdate()
     {
@@ -70,9 +81,58 @@ public class Player : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            
                 Instantiate(bulletPrefab, firePivot.position, firePivot.rotation);
         }
     }
+
+    void DetectDoor()
+    {
+        float rayLength = 0.8f;
+        Ray2D ray = new Ray2D(transform.position, inputVector);
+
+        Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.green);
+
+        RaycastHit2D hit;
+
+        hit = Physics2D.Raycast(transform.position, inputVector, rayLength);
+
+        if(hit.collider.tag == "Door" && inputVector.magnitude != Mathf.Sqrt(2))
+        {
+            MovePlayerToRoom();
+            Vector3 actualCamPosition = gameCamera.transform.position;
+            Vector3 targetCamPosition = actualCamPosition + Vector3.Scale(inputVector, gameCameraVector);
+
+            Debug.Log("Colision");
+            StartCoroutine(MoveCamera(actualCamPosition,targetCamPosition,lerpDuration));
+            
+              
+        }
+    }
+
+    void MovePlayerToRoom()
+    {
+        Vector3 actualPlayerPosition = transform.position;
+        Vector3 advance = new Vector3(3, 3, actualPlayerPosition.z);
+        Vector3 targetPosition = actualPlayerPosition + Vector3.Scale(inputVector, advance);
+        transform.position = targetPosition;
+    }
+    IEnumerator MoveCamera(Vector3 start, Vector3 target, float lerpDuration)
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < lerpDuration)
+        {
+            gameCamera.transform.position = Vector3.Lerp(start, target, curve.Evaluate(elapsedTime / lerpDuration));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        gameCamera.transform.position = target;
+
+    }
+
+
     #endregion
+
+
 }
