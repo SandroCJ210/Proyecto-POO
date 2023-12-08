d6c5c9a521fd38b308d4c8e4257a62ad53b8719e
SandroCJ210 <sandro.carrillo.j@uni.pe>
2023-12-08 18:04:47 -0500

    Update

--- Proyecto-POO/Assets/Scripts/Player.cs
+++ Proyecto-POO/Assets/Scripts/Player.cs
@@ -16,12 +16,18 @@ public class Player : MonoBehaviour
     private Vector2 movementVector;
     public Vector2 InputVector { get; private set; }
     private Vector2 smoothVelocity;
+    public Vector2 directionToMove;
+
     #endregion
     #region State Variables
+    public HealthHeartBar healthHeartBar;
+    private float health, maxHealth;
     #endregion
     #region Events
     private void Start()
     {
+        maxHealth = 6;
+        health = maxHealth;
         rb = GetComponent<Rigidbody2D>();
     }
     private void Update()
@@ -39,15 +45,66 @@ public class Player : MonoBehaviour
     {
         xAxis = Input.GetAxisRaw("Horizontal");
         yAxis = Input.GetAxisRaw("Vertical");
-        
+
         InputVector = new Vector2(xAxis, yAxis).normalized;
     }
     private void Move()
     {
-        rb.velocity = new Vector2(movementVector.x,movementVector.y) * speed;
+        rb.velocity = new Vector2(movementVector.x, movementVector.y) * speed;
         if (rb.velocity.magnitude < 1)
         {
             rb.velocity = Vector2.zero;
+        }
+    }
+    public float GetHealth()
+    {
+        return health;
+    }
+    public float GetMaxHealth()
+    {
+        return maxHealth;
+    }
+    public void TakeDamage()
+    {
+        if (health > 0)
+        {
+            health -= 1;
+            if (health < 0)
+            {
+                //GameOverScreen and reload level
+            }
+            healthHeartBar.DrawHearts();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            TakeDamage();
+            healthHeartBar.DrawHearts();
+        }
+
+        if (collision.tag == "BDD")
+        {
+            directionToMove = Vector2.down;
+            Debug.Log(Vector2.down);
+        }
+        if (collision.tag == "BDU")
+        {
+            directionToMove = Vector2.up;
+            Debug.Log(Vector2.up);
+
+        }
+        if (collision.tag == "BDR")
+        {
+            directionToMove = Vector2.right;
+            Debug.Log(Vector2.right);
+
+        }
+        if (collision.tag == "BDL")
+        {
+            directionToMove = Vector2.left;
+            Debug.Log(Vector2.left);
         }
     }
     #endregion
