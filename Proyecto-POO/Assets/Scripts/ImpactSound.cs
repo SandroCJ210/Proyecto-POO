e1abb7f23a4e91c8cd176b01da56f292bbcc2fbc
SandroCJ210 <sandro.carrillo.j@uni.pe>
2023-12-09 02:38:05 -0500

    .

--- Proyecto-POO/Assets/Scripts/ImpactSound.cs
+++ Proyecto-POO/Assets/Scripts/ImpactSound.cs
@@ -5,16 +5,11 @@ using UnityEngine;
 public class ImpactSound : MonoBehaviour
 {
     private AudioSource audioSource;
-    // Start is called before the first frame update
+
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         audioSource.Play();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
