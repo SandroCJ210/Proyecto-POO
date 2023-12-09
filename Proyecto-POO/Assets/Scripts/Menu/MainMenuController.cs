7250eb7ea6d4696a95312562e2451512ef665e19
SandroCJ210 <sandro.carrillo.j@uni.pe>
2023-12-08 21:02:20 -0500

    Update

---
+++ Proyecto-POO/Assets/Scripts/Menu/MainMenuController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuController : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartLevel();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+    }
+    public void StartLevel(){
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
